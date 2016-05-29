43c57bf558006a307aa68d0ff9d3335ee2cd868f
Jake <jadelgren2012@gmail.com>
2016-05-28 19:21:47 -0700

    Added text boxes, labels, and button to form.

--- GearSharp/Form1.cs
+++ GearSharp/Form1.cs
@@ -16,5 +16,20 @@ namespace GearSharp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
